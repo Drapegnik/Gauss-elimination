d14f5454c8d70081060768169092e120dc24eba3
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2016-11-14 10:29:14 +0300

    add script for matrix generating

---
+++ gen.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import random
+import sys
+
+"""
+usage: python gen.py {matrix_size} {max_element_value}
+return: A | b matrix, where rank A = {matrix_size}
+"""
+
+f = open('input.txt', 'w')
+n = int(sys.argv[1])
+f.write('{}\n'.format(n))
+for i in range(n):
+    for j in range(n+1):
+        template = '{} ' if j != n else '{}\n'
+        f.write(template.format(random.randint(0, int(sys.argv[2]))))
+f.close()
